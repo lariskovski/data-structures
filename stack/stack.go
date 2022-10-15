14cdf83bd7421dcafb90444f131ecc803cc870e4
lariskovski <larissaporto@live.com>
2022-10-15 12:29:14 -0300

    Refactor stack to use nodes struct

--- stack/stack.go
+++ stack/stack.go
@@ -2,14 +2,39 @@ package stack
 
 const stack_length int = 5;
 const empty int = -1;
-
-var stack [stack_length]int;
 var top int = empty;
+
+var head *Node = nil;
+
+type Node struct {
+	value int;
+	next *Node;
+}
+
+func createNode(value int) *Node{
+	var node *Node = new(Node);
+	node.value = value;
+	node.next = nil;
+	return node;
+}
+
+func insertNodeAtHead(node *Node, head **Node){
+	// fmt.Printf("insert %v at head (currently %v)\n", node.value, (*head).value);
+	// fmt.Printf("head address: %v\n", head);
+	node.next = *head;
+	*head = node;
+}
+
+func removeNodeAtHead(head **Node) *Node{
+	var node *Node = *head;
+	*head = (*head).next;
+	return node;
+}
 
 func Push(value int) bool{
 	if (top < stack_length - 1) {
 		top ++;
-		stack[top] = value;
+		insertNodeAtHead(createNode(value), &head)
 		return true;
 	}
 	return false;
@@ -17,9 +42,8 @@ func Push(value int) bool{
 
 func Pop() int{
 	if (top != empty) {
-		var tmp int = stack[top]
 		top --;
-		return tmp;
+		return removeNodeAtHead(&head).value;
 	}
 	return -1;
 }
